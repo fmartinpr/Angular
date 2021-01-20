f7fb3ef522360d71fa22bd1a663148bb3f244ca7
Fermín Martín García <fmgarcia@hotmail.es>
2021-01-20 17:52:08 +0100

    eliminar

--- webpack-starter-typescript-master/src/class/index.ts
+++
@@ -1,5 +0,0 @@
-/*Al estar en la misma carpeta que los módulos no hace falta 
-para definir en la ruta de donde se encuentra el módulo 
-la carpeta*/
-export {Xmen} from './xmen.class';
-//export {Villano} from './villano.class';
--- webpack-starter-typescript-master/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Xmen } from "./class/xmen.class";
-const woldverine = new Xmen('Spiderman','1234');
-woldverine.imprimir();
-const sumar = (a: number, b: number) =>{
-    return a + b;
-}
