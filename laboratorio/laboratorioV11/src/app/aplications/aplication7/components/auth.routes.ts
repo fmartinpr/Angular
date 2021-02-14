edc0114a83a30c2623a7c2fcbd23e5a4a1c51b0e
Fermín Martín García <fmgarcia@hotmail.es>
2021-02-14 18:11:38 +0100

    Auth0

--- laboratorio/laboratorioV11/src/app/aplications/aplication7/components/auth.routes.ts
+++ laboratorio/laboratorioV11/src/app/aplications/aplication7/components/auth.routes.ts
@@ -1,11 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes, CanActivate } from '@angular/router';
 import { ProtegidaAuth0Component } from './protegida-auth0/protegida-auth0.component';
 import { PreciosAuth0Component } from './precios-auth0/precios-auth0.component';
 import { HomeAuth0Component } from './home-auth0/home-auth0.component';
+import { AuthService } from '../services/auth.service';
+import { AuthGuard } from '../services/auth.guard';
 
-export const  AUTH_ROUTES: Routes = [
+export const AUTH_ROUTES: Routes = [
     { path: 'homeAuth0', component: HomeAuth0Component },
     { path: 'preciosAuth0', component: PreciosAuth0Component },
-    { path: 'protegidaAuth0', component: ProtegidaAuth0Component },
+    {
+        path: 'protegidaAuth0',
+        component: ProtegidaAuth0Component,
+        canActivate: [AuthGuard]
+    },
     { path: '**', pathMatch: 'full', redirectTo: 'homeAuth0' }
 ];
